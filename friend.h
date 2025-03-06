06df389ca39ba1ebea24eda1005008fe75979ff6
Snehil Singh Rokey <79813673+snehilsr91@users.noreply.github.com>
2025-03-06 20:00:56 +0530

    Create friend.h

---
+++ friend.h
@@ -0,0 +1,9 @@
+#ifndef FRIEND_H
+#define FRIEND_H
+
+#include "common.h"
+
+void send_friend_request(const char *from, const char *to);
+void list_friends(const char *username);
+
+#endif // FRIEND_H
